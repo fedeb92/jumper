eec767e8434a59bae0cbd88ed43daa302aa1d194
fedeb92 <47046566+fedeb92@users.noreply.github.com>
2020-04-30 00:51:06 -0300

    subida 1era
    uno se creo sin querer no se si hay problema

---
+++ juego 2d para diseñomatica/Assets/Scripts/prueba.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class prueba : MonoBehaviour
+{
+    public bool punch = false;
+    public float timer = 60;
+    public float kill = 30;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(timer);   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log(timer -= 1);
+            punch = true;
+        }
+        if (timer == kill)
+        {
+            Debug.Log(timer = 100);
+            punch = false;
+        }
+
+        if(punch == true)
+        {
+            Debug.Log(timer -= 1);
+        }
+    }
+
+    
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+    }
+}
