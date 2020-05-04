7de2a9a919a624291c2a34519537103ecda9b387
fedeb92 <47046566+fedeb92@users.noreply.github.com>
2020-05-04 09:03:35 -0300

    cambios 2

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    [SerializeField]
+    [Range(2, 100)]
+    public float speed = 10;
+    private Vector3 TargetPosition;
+    private Vector3 TargetPositionWall;
+    private bool MoveOn = false;
+    public GameObject wall;
+
+    void Start()
+    {
+
+    }
+    
+    void Update()
+    {
+        
+         if (Input.GetMouseButton(0))
+         {
+             SetTargetPosition();
+         }
+        if (MoveOn)
+        {
+            MoveP();
+        }
+        
+    }
+
+    public void SetTargetPosition()
+    {
+        TargetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        TargetPosition.z = transform.position.z;
+
+        MoveOn = true;
+        Debug.Log("hit");
+    }
+    
+    public void MoveP()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, TargetPosition, speed * Time.deltaTime);
+        if (transform.position == TargetPosition)
+        {
+            MoveOn = false;
+        }
+    }
+    
+
+
+}
+
+
+
---
+++ Assets/Scripts/Wall.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Wall : MonoBehaviour
+
+{
+    // Start is called before the first frame update
+    
+    [SerializeField]
+    [Range(2, 100)]
+    public float speed = 10;
+    private Vector3 TargetPosition;
+    private Vector3 TargetPositionWall;
+    private bool MoveOn = false;
+    public GameObject bla;
+
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+        if (Input.GetMouseButton(0))
+        {
+            SetTargetPosition();
+            prueba();
+        }
+        
+        if (MoveOn)
+        {
+            MoveP();
+        }
+        
+    }
+
+    public void SetTargetPosition()
+    {
+        TargetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        TargetPosition.z = transform.position.z;
+
+        MoveOn = true;
+        
+    }
+   
+    public void MoveP()
+    {
+         Vector3.MoveTowards(transform.position, TargetPosition, speed * Time.deltaTime);
+        if (transform.position == TargetPosition)
+        {
+            MoveOn = false;
+        }
+    }
+
+    public void prueba()
+    {
+        Debug.Log("fede cool");
+    }
+
+    public void OnPointerClick(PointerEventData data)
+    {
+        Debug.Log("this work");
+    }
+
+}
